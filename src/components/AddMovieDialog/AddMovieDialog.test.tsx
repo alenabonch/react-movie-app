8ee948060d0c1f9cfb73886e045ac7e03b43ad8c
Elena Vinokurova <Elena.Vinokurova@iii.com>
2023-10-13 11:31:44 +0200

    Fixed tests and added more test cases

--- src/components/AddMovieDialog/AddMovieDialog.test.tsx
+++ src/components/AddMovieDialog/AddMovieDialog.test.tsx
@@ -2,6 +2,7 @@ import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import React from 'react';
 import { createMemoryRouter, Outlet, RouterProvider } from 'react-router-dom';
+import { movieMock } from '../../mocks/Movie';
 import AddMovieDialog from './AddMovieDialog';
 
 const mockedUseNavigateWithQuery = jest.fn();
@@ -13,6 +14,7 @@ const mockedFetch = jest.fn();
 jest.mock('../../hooks/useFetch', () => ({
   useFetch: () => ([mockedFetch, null, null]),
 }));
+
 const mockOnAdd = jest.fn();
 const routes = [
   {
@@ -43,19 +45,22 @@ describe(AddMovieDialog, () => {
     expect(mockedUseNavigateWithQuery).toHaveBeenCalledWith('/')
   });
 
-  // it('should call fetch on form submit', async () => {
-  //   const router = createMemoryRouter(routes, {initialEntries: ['/', '/test']});
-  //   render(<RouterProvider router={router} />);
-  //   await user.type(screen.getByLabelText('Title'), 'Test Movie');
-  //   await user.type(screen.getByLabelText('Release Date'), '2020-12-23');
-  //   await user.type(screen.getByLabelText('Movie Url'), 'https://example.com/movie.jpg');
-  //   await user.type(screen.getByLabelText('Rating'), '8.5');
-  //   await user.click(screen.getByLabelText('Genre'));
-  //   await user.click(screen.getByText('Drama'));
-  //   await user.type(screen.getByLabelText('Duration'), '120');
-  //   await user.type(screen.getByLabelText('Overview'), 'Test movie overview');
-  //
-  //   await user.click(screen.getByText('Submit'));
-  //   expect(mockedFetch).toHaveBeenCalled();
-  // });
+  it('should call fetch on form submit', async () => {
+    mockedFetch.mockResolvedValue(movieMock);
+    const router = createMemoryRouter(routes, {initialEntries: ['/', '/test']});
+    render(<RouterProvider router={router} />);
+    await user.type(screen.getByLabelText('Title'), 'Test Movie');
+    await user.type(screen.getByLabelText('Release Date'), '2020-12-23');
+    await user.type(screen.getByLabelText('Movie Url'), 'https://example.com/movie.jpg');
+    await user.type(screen.getByLabelText('Rating'), '8.5');
+    await user.click(screen.getByLabelText('Genre'));
+    await user.click(screen.getByText('Drama'));
+    await user.type(screen.getByLabelText('Duration'), '120');
+    await user.type(screen.getByLabelText('Overview'), 'Test movie overview');
+
+    await user.click(screen.getByText('Submit'));
+    expect(mockedFetch).toHaveBeenCalled();
+    expect(mockedUseNavigateWithQuery).toHaveBeenCalledWith('/1')
+    expect(mockOnAdd).toHaveBeenCalledWith(movieMock)
+  });
 });
--- src/components/EditMovieDialog/EditMovieDialog.test.tsx
+++ src/components/EditMovieDialog/EditMovieDialog.test.tsx
@@ -2,8 +2,7 @@ import { render, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import React from 'react';
 import { createMemoryRouter, Outlet, RouterProvider } from 'react-router-dom';
-import { movieMock } from '../../mocks/Movie';
-import { Movie } from '../../models/Movie';
+import { movieMock, movieMock2 } from '../../mocks/Movie';
 import EditMovieDialog from './EditMovieDialog';
 
 const mockedUseNavigateWithQuery = jest.fn();
@@ -11,7 +10,7 @@ jest.mock('../../hooks/useNavigateWithQuery', () => ({
   useNavigateWithQuery: () => ({navigateWithQuery: mockedUseNavigateWithQuery}),
 }));
 
-const mockedFetch = jest.fn();
+const mockedFetch = jest.fn()
 jest.mock('../../hooks/useFetch', () => ({
   useFetch: () => ([mockedFetch, null, null]),
 }));
@@ -34,29 +33,35 @@ const routes = [
 describe(EditMovieDialog, () => {
   const user = userEvent.setup();
 
-  it('should render add movie form', async () => {
+  it('should render edit movie dialog', async () => {
     const router = createMemoryRouter(routes, {initialEntries: ['/', '/test']});
-    render(<RouterProvider router={router} />);
-    // await expect(screen.getByText('Edit Movie')).toBeInTheDocument();
+    await render(<RouterProvider router={router} />);
 
-    // await waitFor(() => {
-    //   const submitButton = (screen.getByRole('button', {name: 'Submit'}));
-    //   expect(submitButton).toBeInTheDocument();
-    // });
+    await waitFor(() => {
+      expect(screen.getByText('Edit Movie')).toBeInTheDocument();
+      expect(screen.getByText('Submit')).toBeInTheDocument();
+    });
   });
 
-  // it('should navigate to home on close', async () => {
-  //   const router = createMemoryRouter(routes, {initialEntries: ['/', '/test']});
-  //   render(<RouterProvider router={router} />);
-  //   await user.click(screen.getByLabelText('Close Dialog', {selector: 'button'}));
-  //   expect(mockedUseNavigateWithQuery).toHaveBeenCalledWith('/')
-  // });
+  it('should navigate to home on close', async () => {
+    const router = createMemoryRouter(routes, {initialEntries: ['/', '/test']});
+    render(<RouterProvider router={router} />);
+    await waitFor(async () => {
+      await user.click(screen.getByLabelText('Close Dialog', {selector: 'button'}));
+      expect(mockedUseNavigateWithQuery).toHaveBeenCalledWith('/')
+    });
+  });
 
-  // it('should call fetch on form submit', async () => {
-  //   const router = createMemoryRouter(routes, {initialEntries: ['/', '/test']});
-  //   render(<RouterProvider router={router} />);
-  //
-  //   await user.click(screen.getByText('Submit'));
-  //   expect(mockedFetch).toHaveBeenCalled();
-  // });
+  it('should call fetch on form submit', async () => {
+    mockedFetch.mockResolvedValue(movieMock2);
+    const router = createMemoryRouter(routes, {initialEntries: ['/', '/test']});
+    render(<RouterProvider router={router} />);
+
+    await waitFor(async () => {
+      await user.click(screen.getByText('Submit'));
+      expect(mockedFetch).toHaveBeenCalled();
+      expect(mockedUseNavigateWithQuery).toHaveBeenCalledWith('/2')
+      expect(mockOnEdit).toHaveBeenCalledWith(movieMock2)
+    });
+  });
 });
